503fcd40bc22cdca53f1be7f1b30f85b281484ae
Roberto Torres <roberto.torres@mavs.uta.edu>
2019-07-18 02:50:44 -0500

    Add recursive calls to check adjacent cells;

--- minesweeper.js
+++ minesweeper.js
@@ -1,6 +1,7 @@
 var mineField;
+var size;
 function newGame(){
-    var size = Number(document.getElementById("textSize").value);
+    size = Number(document.getElementById("textSize").value);
     var numBombs= Number(document.getElementById("textBombs").value);
     //Check that size is a positive number
     if(!isNaN(size) && !isNaN(numBombs)){
@@ -16,11 +17,11 @@ function newGame(){
                 for(var j=0; j < size; j++)
                 {
                     strHtml += '<td><button class="button-cell" custAttr="uc" onClick="checkMine(' 
-                    + i.toString() + ',' + j.toString() + ', this)" id="buttonMine' + i.toString() + '-' + j.toString() + '" ></button></td>';
+                    + i.toString() + ',' + j.toString() + ')" id="buttonMine' + i.toString() + '-' + j.toString() + '" ></button></td>';
                 }
                 strHtml += '</tr>'
             }
-            createMineField(size, numBombs);
+            createMineField(numBombs);
 
             //Display playing field
             document.getElementById("tableField").innerHTML = strHtml;
@@ -41,26 +42,32 @@ function newGame(){
     }
 }
 
-function checkMine(row, col, btnClicked){
+function checkMine(row, col){
     var strOutput = row.toString() + ', ' + col.toString();
+    if(row < 0 || col < 0 || row >= size || col >= size){
+        return;
+    }
+    var btnClicked = document.getElementById("buttonMine"+(row).toString()+'-'+(col).toString());
+    if(btnClicked.style.backgroundColor !== ""){
+        return;
+    }
     //alert(btnClicked);
     if(mineField[row][col] === "X"){
         btnClicked.style.backgroundColor = "red";
     }
     else{
         btnClicked.style.backgroundColor = "white";
-        btnClicked.innerHTML = mineField[row][col];
+        if(mineField[row][col] === 0){
+            //Call function to clear all adjacent 0's
+            checkMine(row+1, col);
+            checkMine(row-1, col);
+            checkMine(row, col+1);
+            checkMine(row, col-1);
+        }
+        else{
+            btnClicked.innerHTML = mineField[row][col];
+        }
     }
-    /*
-    if(btnClicked.getAttribute("custAttr") !== "red"){
-        btnClicked.style.backgroundColor = "red";
-        btnClicked.setAttribute("custAttr", "red")
-    }
-    else{
-        btnClicked.style.backgroundColor = "";
-        btnClicked.setAttribute("custAttr", "");
-    }
-    */
 }
 
 function endGame() {
@@ -82,7 +89,7 @@ function getRandInt(max){
     return Math.floor(Math.random() * max);
 }
 
-function createMineField(size, numBombs){
+function createMineField(numBombs){
     mineField = new Array(size);
     for(var i = 0; i < size; i++){
         mineField[i] = new Array(size);
@@ -92,13 +99,13 @@ function createMineField(size, numBombs){
     }
     for (var i = 0; i < numBombs; i++){
         //mineField[getRandInt(size)][getRandInt(size)] = "X";
-        insertMine(getRandInt(size), getRandInt(size), size);
+        insertMine(getRandInt(size), getRandInt(size));
     }
 }
 
-function insertMine(row, col, size){
+function insertMine(row, col){
     if(mineField[row][col] === "X"){
-        insertMine(getRandInt(size), getRandInt(size), size);
+        insertMine(getRandInt(size), getRandInt(size));
     }
     else{
         mineField[row][col] = "X"
